0061426d7e00f999b09b998c9bf0e3f19ca575d5
Orion Lawlor <lawlor@alaska.edu>
2021-06-17 02:18:49 -0800

    Slowly bringing new models into Unity.

---
+++ Assets/Mining/ExcahaulerDriveSide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcahaulerDriveSide : MonoBehaviour
+{
+    public float motorTorque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motorTorque=0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Mining/ExcahaulerWheelDriver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcahaulerWheelDriver : MonoBehaviour
+{
+    public ExcahaulerDriveSide side;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Get our hinge joint, apply side.motorTorque.
+        float torque=side.motorTorque;
+    }
+}
